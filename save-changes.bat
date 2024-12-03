37d1e8766d59a08b9dc2381d04773d4a293ce574
Lysboi <yskm.duruk@gmail.com>
2024-12-04 02:11:48 +0300

    Auto save: Çar 04.12.2024  2:11:48,49

--- save-changes.bat
+++ save-changes.bat
@@ -1,7 +1,25 @@
 @echo off
 echo Git changes saving started...
 git add .
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error during git add
+    pause
+    exit /b %ERRORLEVEL%
+)
+
 git commit -m "Auto save: %date% %time%"
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error during git commit
+    pause
+    exit /b %ERRORLEVEL%
+)
+
 git push
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error during git push
+    pause
+    exit /b %ERRORLEVEL%
+)
+
 echo Done!
 pause
